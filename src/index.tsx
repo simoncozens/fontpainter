ccb00121b3fd35710f9022956561a0e03411f89d
Simon Cozens <simon@simon-cozens.org>
2023-07-21 11:26:10 +0100

    Final vestiges of fontwriter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import hbjs from "./hbjs";
-import __wbg_init from 'fontwriter';
 
 const rootElement = document.getElementById('root');
 // const root = ReactDOM.createRoot(rootElement!);
@@ -18,7 +17,6 @@ fetch(`${process.env.PUBLIC_URL}/harfbuzz.wasm`)
     const hb = hbjs(results.instance); // Dirty but works
     window.harfbuzz = results.instance;
     window.hbjs = hb;
-    __wbg_init()
    }).then(() => {
     ReactDOM.render(
         <App />,
